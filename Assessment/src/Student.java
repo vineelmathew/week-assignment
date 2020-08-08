1f36fb795f9337165c9e5a89da77556c58748e2c
Vineel Mathew <vineelrudrapati9@gmail.com>
2020-08-08 10:19:13 +0530

    added hashcode

--- Assessment/src/Student.java
+++ Assessment/src/Student.java
@@ -1,11 +1,10 @@
-import javax.swing.*;
 import java.util.Objects;
 
 public class Student {
-    private int rollno;
+    private String rollno;
     private String name;
     private int age;
-    public Student(int rollno,String name,int age)
+    public Student(String rollno, String name, int age)
     {
         this.rollno=rollno;
         this.name=name;
@@ -13,28 +12,53 @@ public class Student {
     }
     public Student()
     {
-        this(1,"A",18);
+
+        this("1","A",18);
     }
 
-    public int getRollno() {
+    public String getRollno() {
+
         return rollno;
     }
 
-    public void setRollno(int rollno) {
+    public void setRollno(String rollno) {
+
         this.rollno = rollno;
     }
 
     public String getName() {
+
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
     public int getAge() {
         return age;
     }
     public void setAge(int age) {
+
         this.age = age;
+    }
+    public int hashCode()
+    {
+        int rollhash=rollno.hashCode();
+        return rollhash;
+    }
+    @Override
+    public boolean equals(Object Arg) {
+        if(Arg==this)
+        {
+            return true;
+        }
+       if(Arg==null || !(Arg instanceof Student))
+       {
+           return false;
+       }
+       Student that=(Student)Arg;
+       boolean isequal=this.rollno.equals(that.rollno);
+       return isequal;
     }
 }
--- Assessment/src/StudentMain.java
+++ Assessment/src/StudentMain.java
@@ -2,21 +2,21 @@ import java.util.*;
 
 public class StudentMain {
     Map<String,Student> studentmap=new HashMap<>();
-    Set<Integer>evenage=new HashSet<>();
-    Set<Integer>oddage=new HashSet<>();
-    List<Integer>rollnumber=new ArrayList<>();
+    Set<Student>evenage=new HashSet<>();
+    Set<Student>oddage=new HashSet<>();
+    List<String> rollnumber=new ArrayList<String>();
     public static void main(String[] args) {
         StudentMain stuobj=new StudentMain();
         stuobj.app();
     }
     public void app()
     {
-        Student student1=new Student(51,"vineel",22);
-        Student student2=new Student(50,"rudrapati",24);
-        Student student3=new Student(51,"leo",27);
-        Student student4=new Student(50,"david",28);
-        Student student5=new Student(51,"mathew",29);
-        Student student6=new Student(50,"smith",31);
+        Student student1=new Student("51","vineel",22);
+        Student student2=new Student("50","rudrapati",24);
+        Student student3=new Student("51","leo",27);
+        Student student4=new Student("50","david",28);
+        Student student5=new Student("51","mathew",29);
+        Student student6=new Student("50","smith",31);
         studentmap.put(student1.getName(),student1);
         studentmap.put(student2.getName(),student2);
         studentmap.put(student3.getName(),student3);
@@ -26,18 +26,17 @@ public class StudentMain {
         for(String itervar:studentmap.keySet())
         {
             Student data=studentmap.get(itervar);
-            int age= data.getAge();
-            int roll=data.getRollno();
-            if(age%2==0)
+            if(data.getAge()%2==0)
             {
-
-                evenage.add(age);
+                evenage.add(data);
             }
-            else {
-                oddage.add(age);
+            else
+            {
+                oddage.add(data);
             }
+            rollnumber.add(data.getRollno());
         }
-        
+
     }
 }
 
